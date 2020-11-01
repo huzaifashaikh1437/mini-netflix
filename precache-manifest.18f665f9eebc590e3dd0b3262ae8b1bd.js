3458088b0c55d744d41374e2956a309b8ba06b64
huzaifa <shaikhhuzaifa072@gmail.com>
2020-11-01 12:58:37 +0500

    Updates

---
+++ precache-manifest.18f665f9eebc590e3dd0b3262ae8b1bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22054baff1df0c26d85ba16a9a71d74f",
+    "url": "/mini-netflix/index.html"
+  },
+  {
+    "revision": "36f48d6c5b1c53733069",
+    "url": "/mini-netflix/static/css/main.73da1ed7.chunk.css"
+  },
+  {
+    "revision": "bffb32c77e41a6b80363",
+    "url": "/mini-netflix/static/js/2.6d96028f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mini-netflix/static/js/2.6d96028f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f48d6c5b1c53733069",
+    "url": "/mini-netflix/static/js/main.694ea212.chunk.js"
+  },
+  {
+    "revision": "fe710ba20bac3f322293",
+    "url": "/mini-netflix/static/js/runtime-main.e1c308e2.js"
+  }
+]);
